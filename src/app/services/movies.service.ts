bfd4835ed0593f3d4f5303df1d130002edea82d7
Akram <akrammoussaoui14@gmail.com>
2019-10-19 00:10:55 +0100

    setting service

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+@Injectable({
+  providedIn: "root"
+})
+export class MoviesService {
+  private url: string = "https://api.themoviedb.org/3/movie/";
+  private api_key: string = "43b746b767edc8522cb6200aa1821bcb";
+
+  constructor(private httpClient: HttpClient) {}
+
+  getPopularMovies(langage: string, page: string): Observable<any> {
+    const params = new HttpParams()
+      .set("api_key", this.api_key)
+      .set("langage", langage)
+      .set("page", page);
+    return this.httpClient.get(`${this.url}popular`, { params }).pipe(
+      map(data => {
+        return data;
+      })
+    );
+  }
+}
