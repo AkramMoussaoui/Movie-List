6dba742fb9cdc0507e2886dee88a2f83d2d5cf16
Akram <akrammoussaoui14@gmail.com>
2019-10-19 12:08:29 +0100

    adding header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PopularMovieComponent } from './popular-movie/popular-movie.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PopularMovieComponent
+    PopularMovieComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
