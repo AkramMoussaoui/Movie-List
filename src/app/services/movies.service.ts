51e1a9a06cde5c74fc1800247d637f7aaf0d292f
Akram <akrammoussaoui14@gmail.com>
2019-10-19 20:00:23 +0100

    adding types to service

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
+import { IMovies, IPopular } from "../Popular";
 @Injectable({
   providedIn: "root"
 })
@@ -11,14 +12,14 @@ export class MoviesService {
 
   constructor(private httpClient: HttpClient) {}
 
-  getPopularMovies(langage: string, page: string): Observable<any> {
+  getPopularMovies(langage: string, page: string): Observable<IMovies[]> {
     const params = new HttpParams()
       .set("api_key", this.api_key)
       .set("langage", langage)
       .set("page", page);
-    return this.httpClient.get(`${this.url}popular`, { params }).pipe(
+    return this.httpClient.get<IPopular>(`${this.url}popular`, { params }).pipe(
       map(data => {
-        return data;
+        return data.results;
       })
     );
   }
