b34e1916777b20e1983e1ec50519b840ce6dc0b5
Akram <akrammoussaoui14@gmail.com>
2019-10-19 22:40:23 +0100

    adding movie detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { PopularMovieComponent } from "./popular-movie/popular-movie.component";
+import { MovieDetailsComponent } from "./movie-details/movie-details.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   {
     path: "home",
     component: PopularMovieComponent
+  },
+  {
+    path: "movie/:id",
+    component: MovieDetailsComponent
   }
 ];
 
