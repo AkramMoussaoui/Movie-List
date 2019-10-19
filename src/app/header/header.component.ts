8573f4e5e8d7b856020447f587aa7319e95d15f1
Akram <akrammoussaoui14@gmail.com>
2019-10-19 21:30:58 +0100

    setting search feature

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { FilterService } from "../services/filter.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private filter: FilterService) {}
+  listFilter: string;
+  ngOnInit() {}
+  onClick(): void {
+    this.filter.changeMessage(this.listFilter);
   }
-
 }
--- src/app/popular-movie/popular-movie.component.ts
+++ src/app/popular-movie/popular-movie.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { MoviesService } from "../services/movies.service";
 import { IMovies } from "../Popular";
+import { FilterService } from "../services/filter.service";
 
 @Component({
   selector: "app-popular-movie",
@@ -9,12 +10,31 @@ import { IMovies } from "../Popular";
 })
 export class PopularMovieComponent implements OnInit {
   movies: IMovies[];
+  filteredMovies: IMovies[];
+  listFilter: string;
 
-  constructor(private movieService: MoviesService) {}
+  performFilter(filterBy: string): IMovies[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.movies.filter(
+      (movie: IMovies) =>
+        movie.title.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
+  constructor(
+    private movieService: MoviesService,
+    private filter: FilterService
+  ) {}
 
   ngOnInit() {
     this.movieService.getPopularMovies("en-US", "1").subscribe(data => {
       this.movies = data;
+      this.filteredMovies = data;
+    });
+    this.filter.currentMessage.subscribe(message => {
+      this.listFilter = message;
+      this.filteredMovies = this.listFilter
+        ? this.performFilter(this.listFilter)
+        : this.movies;
     });
   }
 }
