74a672ab1408e3d1b642d6e7544bed8acedb35fc
Akram <akrammoussaoui14@gmail.com>
2019-10-19 20:33:29 +0100

    update popular movie component

--- src/app/popular-movie/popular-movie.component.ts
+++ src/app/popular-movie/popular-movie.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MoviesService } from "../services/movies.service";
+import { IMovies } from "../Popular";
 
 @Component({
-  selector: 'app-popular-movie',
-  templateUrl: './popular-movie.component.html',
-  styleUrls: ['./popular-movie.component.css']
+  selector: "app-popular-movie",
+  templateUrl: "./popular-movie.component.html",
+  styleUrls: ["./popular-movie.component.css"]
 })
 export class PopularMovieComponent implements OnInit {
+  movies: IMovies[];
 
-  constructor() { }
+  constructor(private movieService: MoviesService) {}
 
   ngOnInit() {
+    this.movieService.getPopularMovies("en-US", "1").subscribe(data => {
+      this.movies = data;
+    });
   }
-
 }
