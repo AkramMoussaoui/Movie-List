109790827d60ca56ad408b0ecebfc3adf5854a92
Akram <akrammoussaoui14@gmail.com>
2019-10-19 12:07:06 +0100

    adding bootstrap and popular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PopularMovieComponent } from './popular-movie/popular-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopularMovieComponent
   ],
   imports: [
     BrowserModule,
