1932e1bc76b44bc0450c9636c4a8ef96e612961b
Akram <akrammoussaoui14@gmail.com>
2019-10-21 00:07:32 +0100

    adding navigation page to top rate component

--- src/app/top-rated/top-rated.component.ts
+++ src/app/top-rated/top-rated.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { IMovies } from "../Popular";
 import { MoviesService } from "../services/movies.service";
 import { FilterService } from "../services/filter.service";
+import {
+  ActivatedRoute,
+  Router,
+  RouterEvent,
+  NavigationEnd
+} from "@angular/router";
+import { Subject } from "rxjs";
+import { takeUntil, filter } from "rxjs/operators";
 
 @Component({
   selector: "app-top-rated",
   templateUrl: "./top-rated.component.html",
   styleUrls: ["./top-rated.component.css"]
 })
-export class TopRatedComponent implements OnInit {
+export class TopRatedComponent implements OnInit, OnDestroy {
+  destroyed = new Subject<any>();
   movies: IMovies[];
+  page: number;
   filteredMovies: IMovies[];
   listFilter: string;
 
@@ -22,19 +32,55 @@ export class TopRatedComponent implements OnInit {
   }
   constructor(
     private movieService: MoviesService,
-    private filter: FilterService
+    private filter: FilterService,
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
-    this.movieService.getTopRatedMovie("en-US", "1").subscribe(data => {
-      this.movies = data;
-      this.filteredMovies = data;
-    });
+    this.router.events
+      .pipe(
+        filter((event: RouterEvent) => event instanceof NavigationEnd),
+        takeUntil(this.destroyed)
+      )
+      .subscribe(() => {
+        this.page = +this.route.snapshot.params["id"];
+        this.movieService
+          .getTopRatedMovie("en-US", this.page.toString())
+          .subscribe(data => {
+            this.movies = data;
+            this.filteredMovies = data;
+          });
+        this.filter.currentMessage.subscribe(message => {
+          this.listFilter = message;
+          this.filteredMovies = this.listFilter
+            ? this.performFilter(this.listFilter)
+            : this.movies;
+        });
+      });
+    this.page = +this.route.snapshot.params["id"];
+    this.movieService
+      .getTopRatedMovie("en-US", this.page.toString())
+      .subscribe(data => {
+        this.movies = data;
+        this.filteredMovies = data;
+      });
     this.filter.currentMessage.subscribe(message => {
       this.listFilter = message;
       this.filteredMovies = this.listFilter
         ? this.performFilter(this.listFilter)
         : this.movies;
     });
+  }
+
+  navigate(sym): void {
+    sym === 0
+      ? this.router.navigate([`/top/${this.page - 1}`])
+      : this.router.navigate([`/top/${this.page + 1}`]);
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
   }
 }
