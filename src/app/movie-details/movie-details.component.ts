17fe57510d56f3062a457d3ece8f0952ce62b26f
Akram <akrammoussaoui14@gmail.com>
2019-10-20 21:26:48 +0100

    setting service and component

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MoviesService } from "../services/movies.service";
+import { ActivatedRoute } from "@angular/router";
+import { IMovieID } from "../Details";
+import { ICredit } from "../Credit";
 
 @Component({
-  selector: 'app-movie-details',
-  templateUrl: './movie-details.component.html',
-  styleUrls: ['./movie-details.component.css']
+  selector: "app-movie-details",
+  templateUrl: "./movie-details.component.html",
+  styleUrls: ["./movie-details.component.css"]
 })
 export class MovieDetailsComponent implements OnInit {
-
-  constructor() { }
+  movie: IMovieID;
+  credit: ICredit;
+  constructor(
+    private movieService: MoviesService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.movieService
+      .getMovieById("en-US", this.route.snapshot.params["id"])
+      .subscribe(data => {
+        this.movie = data;
+      });
+    this.movieService
+      .getMovieCredit(this.route.snapshot.params["id"])
+      .subscribe(data => {
+        this.credit = data;
+      });
   }
-
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -3,6 +3,8 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { IMovies, IPopular } from "../Popular";
+import { IMovieID } from "../Details";
+import { ICredit } from "../Credit";
 @Injectable({
   providedIn: "root"
 })
@@ -23,9 +25,29 @@ export class MoviesService {
       })
     );
   }
-  getMovieById(id: string): Observable<any> {
-    return;
+
+  getMovieById(langage: string, id: string): Observable<IMovieID> {
+    const params = new HttpParams()
+      .set("api_key", this.api_key)
+      .set("langage", langage);
+    return this.httpClient.get<IMovieID>(`${this.url}${id}`, { params }).pipe(
+      map(data => {
+        return data;
+      })
+    );
   }
+
+  getMovieCredit(id: string): Observable<ICredit> {
+    const params = new HttpParams().set("api_key", this.api_key);
+    return this.httpClient
+      .get<ICredit>(`${this.url}${id}/credits`, { params })
+      .pipe(
+        map(data => {
+          return data;
+        })
+      );
+  }
+
   getTopRatedMovie(langage: string, page: string): Observable<IMovies[]> {
     const params = new HttpParams()
       .set("api_key", this.api_key)
